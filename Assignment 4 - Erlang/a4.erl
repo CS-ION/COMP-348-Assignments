77be0be5be5707c3fd7e09d0f6585e4a2abbe544
Ayaan Jilani <59445725+CS-ION@users.noreply.github.com>
2023-06-29 21:07:36 -0400

    Create a4.erl

---
+++ Assignment 4 - Erlang/a4.erl
@@ -0,0 +1,17 @@
+-module(a4).
+-export([cardealership/2, convertprice/2]).
+
+main(_) -> io:fwrite(cardealership("BMW", "eur")).
+
+cardealership(Car, Currency) ->
+    CARS = #{ "BMW" => 15000, "FERRARI" => 17000, "FORD" => 20000 },
+    CP = convertprice(maps:get(Car, CARS, -1), Currency),
+    RoundedPrice = round(CP),
+    io:fwrite("Price: " ++ integer_to_list(RoundedPrice) ++ "\n" ++ "If the price is negative, then the currency is wrong\n" ++ "If the price is -1, then the car does not exist\n").
+
+convertprice(Price, Currency) ->
+    case Currency of
+        "eur" -> Price * 0.92;
+        "inr" -> Price * 82.08;
+        _ -> Price
+    end.
